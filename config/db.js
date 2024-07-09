f2feac3b2d460b1dac39c273f81546a58ff0b1c8
Shanmukh Pulijala <118909253+ShanmukhPulijala16@users.noreply.github.com>
2024-07-09 11:49:05 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const url = process.env.MONGODB_URI || 'mongodb+srv://shanmukhpulijalajunk16:ZqzVSuVXrCM9mD00@cluster0.cx9lvf9.mongodb.net/ERSShan';
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const uri = process.env.MONGODB_URI || 'mongodb+srv://shanmukhpulijalajunk16:ZqzVSuVXrCM9mD00@cluster0.cx9lvf9.mongodb.net/ERSShan';
+        const conn = await mongoose.connect(uri);
         console.log(`Connected to MongoDB ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
